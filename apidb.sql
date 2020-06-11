97841dd425e4a5c51b966bf67cdb92611749b049
Han Yoon Suk <64110645+yshan8573@users.noreply.github.com>
2020-06-12 02:11:51 +0900

    Add files via upload

---
+++ apidb.sql
@@ -0,0 +1,17 @@
+create table RareMdcinInfoService(
+    RARITY_DRUG_APPOINT_NO varchar2(2000),
+    MANUFPLACE_NAME varchar2(2000),
+    APPLY_NAME varchar2(2000),
+    APPLY_TEL_NO varchar2(2000),
+    MANUFPLACE_ZIP_NO varchar2(2000),
+    MANUFPLACE_ADDR1 varchar2(2000),
+    MANUFPLACE_ADDR2 varchar2(2000),
+    MANUFPLACE_TEL_NO varchar2(2000),
+    PRODT_NAME varchar2(2000),
+    TARGET_DISEASE varchar2(2000),
+    GOODS_NAME varchar2(2000),
+    MANUF_NAME varchar2(2000),
+    APPOINT_DATE varchar2(2000),
+    APPOINT_CANCEL_DATE varchar2(2000),
+    RECEIPT_NO varchar2(2000)
+    );
